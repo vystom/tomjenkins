e130059401bc3dbc47f1ba68904500b872137c6a
vystom <vystom@centrum.cz>
2022-09-20 17:54:44 +0200

    vstup param

--- perl_in_param.pl
+++ perl_in_param.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl -w
 print "*****************************************************************\n";
 print "Vstupni parametry z jenkins a z credentials\n";
-print "*****************************************************************\n";
+print "*****************************************************************\n\n";
 
-print " Param z Jenkins: \n";
+print " Param z Jenkins:\n"; 
+print "   db      : ", $ARGV[0],"\n";
+print "   schema  : ", $ARGV[1],"\n\n";
 
-print " Param ze credantals: \n"
+print " Param ze credantials: \n"; 
+print "   Uzivatel = ", $ARGV[2],"\n";  
+print "   Heslo  = "  , $ARGV[3],"\n";  
 
-print "$ $@ $_ \n";
+
