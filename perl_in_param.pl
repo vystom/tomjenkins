49a068409a17af68aac2fe4679a83f3bb4884e2f
vystom <113784305+vystom@users.noreply.github.com>
2022-09-20 17:04:06 +0200

    Create perl_in_param.pl

---
+++ perl_in_param.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+print "*****************************************************************\n";
+print "Vstupni parametry z jenkins a z credentials\n";
+print "*****************************************************************\n";
+
+print " Param z Jenkins: \n";
+
+print " Param ze credantals: \n"
+
+print "$ $@ $_ \n";
